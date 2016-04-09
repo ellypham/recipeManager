98faf4300427ac11fdcc5629076a4cc812f2429a
ellypham <ellypham@gmail.com>
2016-04-09 17:19:44 +0000

    initializing

---
+++ app/models/recipe_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+    
+    def setup
+        @recipe = Recipe.new(name:"chicken parm", summary: "this is the best chicken parm recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
+    end
+    
+    test "recipe should be valid" do
+        assert @recipe.valid?
+    end
+end
